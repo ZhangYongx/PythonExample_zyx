27d52345a365e80362e1d14baa863a13efde3618
ZhangYongxu <ZhangYongx@users.noreply.github.com>
2017-03-24 16:31:45 +0800

    Update 批量更换后缀名.py

--- 批量更换后缀名.py
+++ 批量更换后缀名.py
@@ -7,5 +7,5 @@ for file in os.listdir('.'):
     if os.path.isfile(file):
         posion = os.path.splitext(file)
         if posion[1] == 'mp4':    #如果文件的后缀名为mp4
-            newname = posion[0] + '.py' #把后缀名改为py
-            os.rename(file,newname)
+            newname = posion[0] + '.py' #把后缀名改为py。由于是直接添加后缀，也不用考虑文件名是中午的情况
+            os.rename(file,newname)   
